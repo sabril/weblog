fb0820b82bf5522054e3e849e65dd48abd14cbd4
Muhammad Syaiful Sabril <syaiful.sabril@gmail.com>
2012-09-17 16:54:08 +0700

    pertamax

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myblog::Application.config.secret_token = 'ea59fd3ec0014d067282a40766a4ee99f18c2b1fef670124557f4a740d82983463ea637ff8dee215c490eb73489803c5a0693310588b71677beb2a7de51e5019'
